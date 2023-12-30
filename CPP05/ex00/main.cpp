9a5d98360639d81edefb30c423f3506154dd488a
Niki NIKOLOV <ngenadie@made-f0Cr8s1.clusters.42paris.fr>
2023-12-30 16:09:30 +0100

    ex00 better main

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -4,30 +4,35 @@ int main()
 {
     Bureaucrat test("TEST", 1);
     Bureaucrat test2("TEST2", 150);
-    //try 
-    //{ Bureaucrat niki("NIKI", 151); }
-    //catch (std::exception& me)
-    //{ std::cout << "Caught an Exeption" << std::endl; 
-    //  std::cout << me.what() << std::endl; }
-    //try 
-    //{ Bureaucrat niki("NIKI", 0); }
-    //catch (std::exception& me)
-    //{ std::cout << "Caught an Exeption" << std::endl; 
-    //  std::cout << me.what() << std::endl; }
-    //try 
-    //{ Bureaucrat niki("NIKI", 12); }
-    //catch (std::exception& me)
-    //{ std::cout << "Caught an Exeption" << std::endl; 
-    //  std::cout << me.what() << std::endl; }
-    //std::cout << test;
-    
+    Bureaucrat test3("niki1", 1);
+    Bureaucrat test4("niki2", 150);
     std::cout << test;
     std::cout << test2;
-
-    try 
+    std::cout << test3;
+    std::cout << test4;
+    test3 = test;
+    test4 = test2;
+    std::cout << test3;
+    std::cout << test4;
+    try
+    { Bureaucrat niki("NIKI", 151); }
+    catch (std::exception& me)
+    { std::cout << me.what() << std::endl; }
+    try
+    { Bureaucrat niki("NIKI", 0); }
+    catch (std::exception& me)
+    { std::cout << me.what() << std::endl; }
+    try
     {
       for(unsigned int i = 0; i < 200; i++)
         test.decreaseGrade();
+    }
+    catch(const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+    try 
+    {
       for(unsigned int i = 0; i < 200; i++)
         test2.increaseGrade();
     }
@@ -35,4 +40,6 @@ int main()
     {
       std::cerr << e.what() << '\n';
     }
+    std::cout << test;
+    std::cout << test2;
 }
